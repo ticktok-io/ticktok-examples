38061a7d4e1555603c5dde59acc0eb1091014854
Eli Segal <eli.segal@gmail.com>
2019-05-13 19:16:41 +0300

    updated nodejs example

--- nodejs/src/simple-interval.js
+++ nodejs/src/simple-interval.js
@@ -1,10 +1,10 @@
 const ticktok = require('ticktok');
 
-domain = process.env.TICKTOK_DOMAIN || 'http://localhost:8080';
-token = process.env.TICKTOK_TOKEN || 'ticktok-zY3wpR';
+const domain = process.env.TICKTOK_DOMAIN || 'http://localhost:8080';
+const token = process.env.TICKTOK_TOKEN || 'ticktok-zY3wpR';
 
 ticktok(domain, token)
   .schedule(
     { name: 'simple-interval', schedule: 'every.10.seconds' },
-    () => { console.log('tick') }
+    (err, tick) => { console.log('tick') }
   );
