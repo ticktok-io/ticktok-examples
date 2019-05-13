c1ed30446c94fa5bee3252dcc470a9181fb59cd5
Eli Segal <eli.segal@gmail.com>
2019-05-13 14:03:59 +0300

    updated js version

--- nodejs/src/simple-interval.js
+++ nodejs/src/simple-interval.js
@@ -1,10 +1,10 @@
-const { ticktok, clock } = require('ticktok');
+const ticktok = require('ticktok');
 
 domain = process.env.TICKTOK_DOMAIN || 'http://localhost:8080';
 token = process.env.TICKTOK_TOKEN || 'ticktok-zY3wpR';
 
 ticktok(domain, token)
-  .schedule(clock.named('simple-interval').on('every.10.seconds').invoke(() => console.log('tick')));
-
-
-
+  .schedule(
+    { name: 'simple-interval', schedule: 'every.10.seconds' },
+    () => { console.log('tick') }
+  );
