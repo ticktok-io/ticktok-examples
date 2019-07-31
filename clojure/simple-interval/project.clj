352ef705ec08361952342ab43e8bee5b6b1be48d
Wanishing <>
2019-07-31 09:42:25 +0300

    upgrade ticktok client

--- clojure/simple-interval/project.clj
+++ clojure/simple-interval/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ticktok "1.0.7"]]
+                 [ticktok "1.0.8"]]
   :jvm-opts ~(let [version     (System/getProperty "java.version")
                    [major _ _] (clojure.string/split version #"\.")]
                (if (>= (java.lang.Integer/parseInt major) 9)
