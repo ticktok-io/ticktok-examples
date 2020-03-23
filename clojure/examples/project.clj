27e9ed68c9b9c8730b60ba565d3d25e362b6a7fe
Wanishing <>
2020-03-23 20:31:26 +0200

    updated client

--- clojure/examples/project.clj
+++ clojure/examples/project.clj
@@ -3,10 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ~(let [version     (System/getProperty "java.version")
-                   [major _ _] (clojure.string/split version #"\.")]
-               (if (>= (java.lang.Integer/parseInt major) 9)
-                 ["--add-modules" "java.xml.bind"]
-                 []))
+
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ticktok "1.0.8"]])
+                 [ticktok "1.0.9"]])
